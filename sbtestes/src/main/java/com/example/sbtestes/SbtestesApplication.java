51dd5e1bac6d18575a3742adfac5a8e8afda368f
Geraldo-git <laboratorio.inicial@gmail.com>
2022-05-03 13:42:01 -0300

    Projeto iniciado

---
+++ sbtestes/src/main/java/com/example/sbtestes/SbtestesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbtestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtestesApplication.class, args);
+	}
+
+}
---
+++ sbtestes/src/test/java/com/example/sbtestes/SbtestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
